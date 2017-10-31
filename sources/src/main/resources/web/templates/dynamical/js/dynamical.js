09ae129bad157262c3850705642b14df87b089e9
Mario Serrano <mario@dynamiasoluciones.com>
2017-10-31 16:03:53 -0500

    fix

--- sources/src/main/resources/web/templates/dynamical/js/dynamical.js
+++ sources/src/main/resources/web/templates/dynamical/js/dynamical.js
@@ -1,25 +1,25 @@
 function applyJqueryStuff() {
-	// Activate the layout maker
-	$.AdminLTE.layout.activate();
+    // Activate the layout maker
+    $.AdminLTE.layout.activate();
 
-	// Enable sidebar tree view controls
-	$.AdminLTE.tree('.sidebar');
+    // Enable sidebar tree view controls
+    $.AdminLTE.tree('.sidebar');
 
-	$('a.sidebar-toggle').click(function() {
-		
-		
-		zWatch.fireDown("onSize",'');
-		
-	});
+    $('a.sidebar-toggle').click(function () {
+
+        zWatch.fireDown("onSize", '');
+
+    });
+
 
 }
 
 function onPageLoaded() {
 
-	$('.tooltip').tooltipster({
-		position : 'bottom'
-	});
-	$('.actiontb-a').tooltipster({
-		position : 'bottom'
-	});
+    $('.tooltip').tooltipster({
+        position: 'bottom'
+    });
+    $('.actiontb-a').tooltipster({
+        position: 'bottom'
+    });
 }
